49d7af0081a04db225f502effd57df30dec6b0ae
Oziel Guimarães de Paula Silva <hi@ozielguimaraes.dev>
2023-06-02 07:29:42 -0300

    Initial commit

---
+++ Gtech.Transitions/Gtech.Transitions/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Plugin.SharedTransitions;
+using Xamarin.Forms;
+
+namespace Gtech.Transitions
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new SharedTransitionNavigationPage(new MainPage());
+        }
+
+        //public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        //protected override async void OnInitialized()
+        //{
+        //    InitializeComponent();
+
+        //    //Standard navigationpage
+        //    //await NavigationService.NavigateAsync($"{nameof(SharedTransitionNavigationPage)}/{nameof(MainPage)}");
+
+        //    //Tabbed Page
+        //    //await NavigationService.NavigateAsync(nameof(MainTabbedPage));
+
+        //    //Master-Detail
+        //    //await NavigationService.NavigateAsync(nameof(MainMasterDetailPage));
+
+        //    await NavigationService.NavigateAsync(nameof(MainPage));
+        //}
+
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
